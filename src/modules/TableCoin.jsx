316b0157a0d2a5565c5bc4d6de3faf619bd2e21d
Mohammad Sharifi <mohammad821381@gmail.com>
2025-11-07 01:29:26 +0330

    ...

---
+++ src/modules/TableCoin.jsx
@@ -0,0 +1,12 @@
+function TableCoin({ coins }) {
+  return (
+    <>
+      {console.log(coins)}
+        <div>
+          <h2>Cryptocurrency Prices by Market Cap</h2>
+        </div>
+    </>
+  )
+}
+
+export default TableCoin
---
+++ src/templates/HomePage.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import TableCoin from '../modules/TableCoin'
+import { getCoinList } from '../services/CryptoAPIs'
+
+function HomePage() {
+      const [coins, setCoins] = useState([])
+
+      useEffect(() => {
+            const getData = async () => {
+                  const response = await fetch(getCoinList())
+                  const json = await response.json()
+                  setCoins(json)
+            }
+            getData()
+      }, [])
+      return (
+            <div>
+                  <TableCoin coins = {coins}/>
+            </div>
+      )
+}
+
+export default HomePage
