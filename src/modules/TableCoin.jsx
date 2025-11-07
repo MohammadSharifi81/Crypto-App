1a1e2987c33db51807bb53102d9d4ae3f04df3a7
Mohammad Sharifi <mohammad821381@gmail.com>
2025-11-07 21:38:59 +0330

    styles for table

--- src/modules/TableCoin.jsx
+++ src/modules/TableCoin.jsx
@@ -1,12 +1,14 @@
 import chartUp from '../assets/chart-up.svg'
 import chartDown from '../assets/chart-down.svg'
+import {Rings} from 'react-loader-spinner'
+import styles from './TableCoin.module.css'
 
-function TableCoin({ coins }) {
+function TableCoin({ coins, isLoading }) {
   return (
     <>
-      {console.log(coins)}
-        <div>
-          <table>
+      {isLoading ? (<Rings color = '#3874ff'/>) :  
+      (<div className={styles.container}>
+          <table className = {styles.table}>
             <thead>
               <tr>
                 <th>coin</th>
@@ -19,25 +21,37 @@ function TableCoin({ coins }) {
             </thead>
             <tbody>
               {coins.map((coin) => (
-                <tr key={coin.id}>
-                  <td>
-                    <div>
-                      <img src={coin.image} alt="" />
-                      <span>{coin.symbol.toUpperCase()}</span>
-                    </div>
-                  </td>
-                  <td>{coin.name}</td>
-                  <td>${coin.current_price.toLocaleString()}</td>
-                  <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                  <td>{coin.total_volume.toLocaleString()}</td>
-                  <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt="" /></td>
-                </tr>
+                <TableRow coin = {coin} key = {coin.id} />
               ))}
             </tbody>
           </table>
-        </div>
+        </div>)}
     </>
   )
 }
 
-export default TableCoin
+export default TableCoin;
+
+const TableRow = ({coin 
+  : {image, 
+    symbol, 
+    name, 
+    current_price,
+    price_change_percentage_24h: price_change , 
+    total_volume}}) => {
+  return (
+    <tr>
+      <td>
+        <div className={styles.symbol}>
+          <img src={image} alt="" />
+          <span>{symbol.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td price_change = {price_change > 0 ? styles.success : styles.error}>{price_change.toFixed(2)}%</td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td><img src={price_change > 0 ? chartUp : chartDown} alt="" /></td>
+    </tr>
+  )
+}
